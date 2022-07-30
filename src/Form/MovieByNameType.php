143c524065aab0fe0befb603d00be1fe4681a15f
Yoli35 <ojm16@free.fr>
2022-07-30 12:52:33 +0200

    Headers and paging adjustments

--- src/Form/MovieByNameType.php
+++ src/Form/MovieByNameType.php
@@ -4,10 +4,9 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MovieByNameType extends AbstractType
 {
@@ -20,7 +19,7 @@ class MovieByNameType extends AbstractType
         }
 
         $builder
-            ->add('query', TextType::class, ['required' => false])
+            ->add('query', SearchType::class, ['required' => false])
             ->add('year', ChoiceType::class, ['choices' => $years])
             ->add('save', SubmitType::class)
             ;
