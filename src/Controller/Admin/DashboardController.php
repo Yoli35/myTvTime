0e230ed7dbce061d56dcd397891565a421a8e489
Yoli35 <ojm16@free.fr>
2022-08-09 12:41:40 +0200

    Locale in admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,8 +13,10 @@ use App\Entity\YoutubeVideo;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Provider\AdminContextProvider;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
+
 //use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,12 +24,13 @@ use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class DashboardController extends AbstractDashboardController
 {
-//    private string $locale;
-//
-//    public function __construct(Request $request)
-//    {
-//        $this->locale = $request->getLocale();
-//    }
+    private AdminContextProvider $adminContextProvider;
+
+    public function __construct(AdminContextProvider $adminContextProvider)
+    {
+        $this->adminContextProvider = $adminContextProvider;
+    }
+
     /**
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
@@ -35,8 +38,8 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin')]
     public function index(): Response
     {
-         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-         return $this->redirect($adminUrlGenerator->setController(ArticleCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(ArticleCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -46,6 +49,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+        $context = $this->adminContextProvider->getContext();
+        $locale = $context->getRequest()->getLocale();
+
+
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::section('Users');
         yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
@@ -65,7 +72,7 @@ class DashboardController extends AbstractDashboardController
         ]);
         yield MenuItem::section('Other');
         yield MenuItem::linkToUrl('Homepage', 'fa fa-rocket', '/');
-//        yield MenuItem::linkToUrl('Blog', 'fa fa-rocket', '/'.$this->locale.'/blog');
+        yield MenuItem::linkToUrl('Blog', 'fa fa-rocket', '/'.$locale.'/blog');
         yield MenuItem::linkToLogout('Logout', 'fa fa-rocket');
     }
 }
