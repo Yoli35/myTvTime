47ddb9e4fc1dd57a65c2f909db1892478232c9aa
Yoli35 <ojm16@free.fr>
2022-10-19 13:00:49 +0200

    CSS: dark/light mode for registration and login
    form without bootstrap

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,14 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IdenticalTo;
 use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationFormType extends AbstractType
@@ -27,8 +26,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('username')
+            ->add('email', EmailType::class, [
+                'attr' => ['placeholder' => $this->translator->trans('Email address')]
+            ])
+            ->add('username', TextType::class, [
+                'attr' => ['placeholder' => $this->translator->trans('Choose a username')]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -43,8 +46,8 @@ class RegistrationFormType extends AbstractType
                 'invalid_message' => $this->translator->trans('The password fields must match.'),
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
+                'first_options'  => ['label' => 'Password', 'attr' => ['placeholder' => $this->translator->trans('Enter your password')]],
+                'second_options' => ['label' => 'Repeat Password', 'attr' => ['placeholder' => $this->translator->trans('Confirm your password')]],
             ])
 /*            ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
