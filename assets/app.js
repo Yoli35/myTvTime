71c65a1fb8ed67f5025ff0834c8c54025cd662bb
Yoli35 <ojm16@free.fr>
2023-01-12 15:51:02 +0100

    Add diaporama to movie, serie, event pages

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,23 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import './styles/form.scss';
-import './styles/movie.scss';
-import './styles/user.scss';
-import './styles/youtube.scss';
-import './styles/tiktok.scss';
 import './styles/article.scss';
 import './styles/collection.scss';
-import './styles/serie.scss';
-import './styles/event.scss';
 import './styles/devices.scss';
+import './styles/diaporama.scss';
+import './styles/event.scss';
+import './styles/form.scss';
+import './styles/movie.scss';
+import './styles/serie.scss';
+import './styles/tiktok.scss';
+import './styles/user.scss';
+import './styles/youtube.scss';
 
-import './js/flash';
 import './js/cookie-consent';
+// import './js/diaporama';
+import './js/flash';
 import './js/mydropzone_controller'
 
 // loads the code syntax highlighting library
 import './js/highlight.js';
-
-// import 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
