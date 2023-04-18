33ca266141d305c70dca4a2a03e04f61800feaf8
Yoli35 <ojm16@free.fr>
2023-04-18 12:11:04 +0200

    Chat: last activity ago with timeZone & translation

--- src/Twig/UsersExtension.php
+++ src/Twig/UsersExtension.php
@@ -7,20 +7,33 @@ use App\Repository\UserRepository;
 use DateTimeImmutable;
 use DateTimeZone;
 use Exception;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class UsersExtension extends AbstractExtension
 {
-    public function __construct(private readonly UserRepository $userRepository)
+    public function __construct(
+        private readonly UserRepository      $userRepository,
+        private readonly TranslatorInterface $translator,
+    )
     {
     }
 
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('userList', [$this, 'userList']),
+            new TwigFunction('userList', [$this, 'userList'], ['is_safe' => ['html']]),
+            new TwigFunction('lastActivityAgo', [$this, 'lastActivityAgo'], ['is_safe' => ['html']]),
         ];
+    }
+
+    public function getFilters(): array
+    {
+        return array(
+            new TwigFilter('lastActivityAgo', [$this, 'lastActivityAgo'], ['is_safe' => ['html']]),
+        );
     }
 
     public function userList(): array
@@ -43,5 +56,49 @@ class UsersExtension extends AbstractExtension
                 'isOnLine' => $user->isOnLine($now),
             ];
         }, $users);
+    }
+
+    public function lastActivityAgo($lastActivityAt, $locale = null): string
+    {
+        $timeZone = 'Europe/Paris';
+        try {
+            $date = new DateTimeImmutable('now', new DateTimeZone($timeZone));
+            $lastActivityAt = new DateTimeImmutable($lastActivityAt->format('Y-m-d H:i:s'), new DateTimeZone($timeZone));
+        } catch (Exception) {
+            $date = new DateTimeImmutable();
+        }
+
+        static $units = array(
+            'y' => 'year',
+            'm' => 'month',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second'
+        );
+
+        $diff = date_diff($date, $lastActivityAt);
+        dump($timeZone, $lastActivityAt, $date, $diff);
+
+        foreach ($units as $attribute => $unit) {
+            $count = $diff->$attribute;
+            if (0 !== $count) {
+                return $this->doGetDiffMessage($count, $diff->invert, $unit, $locale);
+            }
+        }
+
+        return $this->getEmptyDiffMessage($locale);
+    }
+
+    protected function doGetDiffMessage(int $count, bool $invert, string $unit, string $locale = null): string
+    {
+        $id = sprintf('diff.%s.%s', $invert ? 'ago' : 'in', $unit);
+
+        return $this->translator->trans($id, array('%count%' => $count), 'time', $locale);
+    }
+
+    public function getEmptyDiffMessage(string $locale = null): string
+    {
+        return $this->translator->trans('diff.empty', array(), 'time', $locale);
     }
 }
