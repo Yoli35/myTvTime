1643c3d9a68966765264fa1d37f9acb5121d3ac2
Yoli35 <ojm16@free.fr>
2023-06-19 18:06:40 +0200

    Global search in db

--- public/js/searchIndex.js
+++ public/js/searchIndex.js
@@ -6,11 +6,18 @@ inputSearch.focus();
 inputSearch.select();
 
 const submitSearch = document.querySelector("#submit");
-submitSearch.addEventListener("click", () => {
+submitSearch.addEventListener("click", submit);
+inputSearch.addEventListener("keyup", function(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        submit();
+    }
+});
+new AnimatedHeader();
+
+function submit(){
     const dbSearch = document.querySelector("#db").checked ? 1:0;
     const urlSearch = document.querySelector("#url").value;
     const query = inputSearch.value;
     window.location.href = `${urlSearch}?query=${query}&page=1&db=${dbSearch}`;
-});
-
-new AnimatedHeader();
+}
