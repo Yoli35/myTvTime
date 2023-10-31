864ff1875a95a5e330f888a8fec2223ae4af348a
Yoli35 <ojm16@free.fr>
2023-10-31 10:38:16 +0100

    DateService: Origin and language of weekdays

--- src/Service/DateService.php
+++ src/Service/DateService.php
@@ -10,38 +10,38 @@ use IntlDateFormatter;
 
 class DateService
 {
-    private $days = [
-        "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",
-        "Londin", "Mårdi", "Mèrkidi", "Djûdi", "Vinrdi", "Sèmedi", "Dimègne",
-        "Lundi", "Mardi", "Mercrédi", "Jéeudi", "Vendrédi", "Sammedi", "Dîmmaunche",
-        "Léndi", "Mardi", "Mécrdi", "Jheùdi", "Vendrdi", "Sémedi", "Dimenche",
-        "Diluns", "Dimars", "Dimècres", "Dijòus", "Divendres", "Dissabte", "Dimenge",
-        "Dilun", "Dimars", "Dimèdre", "Dijòu", "Divèndre", "Dissate", "Dimenche",
-        "Luni", "Marti", "Marcuri", "Ghjovi", "Venneri", "Sabbatu", "Dumenica",
-        "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica",
-        "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo",
-        "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge",
-        "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo",
-        "Luni", "Marţi", "Miercuri", "Joi", "Vineri", "Sâmbătă", "Duminică",
-        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
-        "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag",
-        "Mandi", "Zischdi", "Mittwuch", "Dunnerschdi", "Fridi", "Sàmschdi", "Sunndi",
-        "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag",
-        "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag",
-        "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag",
-        "Mánnudagur", "Þriðjudagur", "Miðvikudagur", "Fimmtudagur", "Föstudagur", "Laugardagur", "Sunnudagur",
-        "Llun", "Mawrth", "Mercher", "Iau", "Gwener", "Sadwrn", "Sul",
-        "Lun", "Meurzh", "Merc'her", "Yaou", "Gwener", "Sadorn", "Sul",
-        "Diluain", "Dimàirt", "Diciadaoin", "Diardaoin", "Dihaoine", "Disathairne", "Didòmhnaich",
-        "Luan", "Máirt", "Céadaoin", "Déardaoin", "Aoine", "Satharn", "Domhnach",
-        "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο", "Κυριακή",
-        "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata", "Igandea",
-        "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai",
-        "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev",
-        "Poniedziałek", "Wtórek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela",
-        "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье",
-        "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena",
-        "Jumatatu", "Jumanne", "Jumatano", "Alhamisi", "Ijumaa", "Jumamosi", "Jumapili",];
+    private array $days = [
+        "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",                                    /* Français - France */
+        "Londin", "Mårdi", "Mèrkidi", "Djûdi", "Vinrdi", "Sèmedi", "Dimègne",                                       /* Picard - France */
+        "Lundi", "Mardi", "Mercrédi", "Jéeudi", "Vendrédi", "Sammedi", "Dîmmaunche",                                /* Wallon - Belgique */
+        "Léndi", "Mardi", "Mécrdi", "Jheùdi", "Vendrdi", "Sémedi", "Dimenche",                                      /* Lorrain - France */
+        "Diluns", "Dimars", "Dimècres", "Dijòus", "Divendres", "Dissabte", "Dimenge",                               /* Occitan - France */
+        "Dilun", "Dimars", "Dimèdre", "Dijòu", "Divèndre", "Dissate", "Dimenche",                                   /* Provençal - France */
+        "Luni", "Marti", "Marcuri", "Ghjovi", "Venneri", "Sabbatu", "Dumenica",                                     /* Corsu - France */
+        "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica",                               /* Italiano - Italie */
+        "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo",                                   /* Español - Espagne */
+        "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge",                            /* Català - Espagne */
+        "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo",         /* Português - Portugal */
+        "Luni", "Marţi", "Miercuri", "Joi", "Vineri", "Sâmbătă", "Duminică",                                        /* Română - Roumanie */
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",                               /* English - Royaume-uni */
+        "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag",                            /* Deutsch - Allemagne */
+        "Mandi", "Zischdi", "Mittwuch", "Dunnerschdi", "Fridi", "Sàmschdi", "Sunndi",                               /* Alemannisch - Allemagne */
+        "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag",                             /* Nederlands - Pays-bas */
+        "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag",                                     /* Dansk - Danemark */
+        "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag",                                      /* Svenska - Suède */
+        "Mánnudagur", "Þriðjudagur", "Miðvikudagur", "Fimmtudagur", "Föstudagur", "Laugardagur", "Sunnudagur",      /* Íslenska - Islande */
+        "Llun", "Mawrth", "Mercher", "Iau", "Gwener", "Sadwrn", "Sul",                                              /* Cymraeg - Pays de Galles */
+        "Lun", "Meurzh", "Merc'her", "Yaou", "Gwener", "Sadorn", "Sul",                                             /* Brezhoneg - Bretagne */
+        "Diluain", "Dimàirt", "Diciadaoin", "Diardaoin", "Dihaoine", "Disathairne", "Didòmhnaich",                  /* Gàidhlig - Écosse */
+        "Luan", "Máirt", "Céadaoin", "Déardaoin", "Aoine", "Satharn", "Domhnach",                                   /* Gaeilge - Irlande */
+        "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο", "Κυριακή",                                 /* Ελληνικά - Grèce */
+        "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata", "Igandea",                    /* Euskara - Pays basque */
+        "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai",                     /* Suomi - Finlande */
+        "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev",                         /* Eesti - Estonie */
+        "Poniedziałek", "Wtórek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela",                             /* Polski - Pologne */
+        "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье",                          /* Русский - Russie */
+        "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena",                /* Latviešu - Lettonie */
+        "Jumatatu", "Jumanne", "Jumatano", "Alhamisi", "Ijumaa", "Jumamosi", "Jumapili",];                          /* Kiswahili - Tanzanie */
 
     public function newDate($dateString, $timeZone, $allDay = false): DateTime
     {
@@ -130,7 +130,7 @@ class DateService
     {
         $days = [];
         $n = count($this->days);
-        if ($count > $n) $count = $n;
+        if ($count > $n / 2) $count = $n / 2;
         for ($i = 0; $i < $count; $i++) {
             do {
                 $day = $this->days[rand(0, $n - 1)];
