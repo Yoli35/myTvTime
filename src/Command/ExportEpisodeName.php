4a8b321338a8d8bd484081a7b527124f800927d0
Yoli35 <ojm16@free.fr>
2024-03-27 11:37:52 +0100

    Export substitute episode names to json command

--- src/Command/ExportEpisodeName.php
+++ src/Command/ExportEpisodeName.php
@@ -2,9 +2,6 @@
 
 namespace App\Command;
 
-use App\Repository\FavoriteRepository;
-use App\Repository\SerieRepository;
-use App\Repository\SerieViewingRepository;
 use App\Repository\UserRepository;
 use App\Service\DateService;
 use App\Service\TMDBService;
@@ -15,7 +12,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\String\Slugger\AsciiSlugger;
 
 #[AsCommand(
     name: 'app:export:episode-names',
@@ -26,8 +22,6 @@ class ExportEpisodeName extends Command
     public function __construct(
         private readonly DateService            $dateService,
         private readonly ManagerRegistry        $registry,
-        private readonly SerieRepository        $serieRepository,
-        private readonly SerieViewingRepository $serieViewingRepository,
         private readonly TMDBService            $tmdbApi,
         private readonly UserRepository         $userRepository,
     )
@@ -59,15 +53,11 @@ class ExportEpisodeName extends Command
             }
         }
         $user = $this->userRepository->find($userId);
-        $slugger = new AsciiSlugger();
 
         if ($userId && $serieId) {
-            $serie = $this->serieRepository->find($serieId);
-            $serieViewings = $this->serieViewingRepository->findBy(['user' => $user, 'serie' => $serie]);
-            $series = $this->getOneSeries($userId, $serieId, $user->getPreferredLanguage() ??'fr');
+            $series = $this->getOneSeries($userId, $serieId, $user->getPreferredLanguage() ?? 'fr');
         } else {
-            $serieViewings = $this->serieViewingRepository->findBy(['user' => $user]);
-            $series = $this->getSeries($userId, $user->getPreferredLanguage() ??'fr');
+            $series = $this->getSeries($userId, $user->getPreferredLanguage() ?? 'fr');
         }
         //dd($series);
         $count = 0;
@@ -83,27 +73,27 @@ class ExportEpisodeName extends Command
                 $line = sprintf("%d - %s", $s['id'], $s['name']);
             $io->writeln($line);
 
-            for ($i=1; $i<=$s['number_of_seasons']; $i++) {
+            for ($i = 1; $i <= $s['number_of_seasons']; $i++) {
                 $io->writeln(sprintf("Season %d", $i));
                 $seasonEpisodesWithSubstituteNames = array_values(array_filter($this->getSeasonEpisodes($userId, $s['id'], $i), fn($episode) => $episode['substitute_name']));
 
-                 if (count($seasonEpisodesWithSubstituteNames) > 0) {
-                     $seasonCount++;
-                     $season = json_decode($this->tmdbApi->getTvSeason($s['tmdb_id'], $i, $user->getPreferredLanguage() ??'fr'), true);
-                     $episodes = $season['episodes'];
-                     foreach ($seasonEpisodesWithSubstituteNames as $episode) {
-                         $episodeCount++;
-                         $tmdbEpisodeId = $this->getEpisodeId($episode['episode_number'], $episodes);
-                         $line = sprintf("S%02dE%02d - %d - %s", $i, $episode['episode_number'], $tmdbEpisodeId, $episode['substitute_name']);
-                         $io->writeln($line);
-                         $substituteNames[] = [
-                             'serie_id' => $s['id'],
-                             'season_number' => $i,
-                             'episode_number' => $episode['episode_number'],
-                             'tmdb_episode_id' => $tmdbEpisodeId,
-                             'substitute_name' => $episode['substitute_name'],
-                         ];
-                     }
+                if (count($seasonEpisodesWithSubstituteNames) > 0) {
+                    $seasonCount++;
+                    $season = json_decode($this->tmdbApi->getTvSeason($s['tmdb_id'], $i, $user->getPreferredLanguage() ?? 'fr'), true);
+                    $episodes = $season['episodes'];
+                    foreach ($seasonEpisodesWithSubstituteNames as $episode) {
+                        $episodeCount++;
+                        $tmdbEpisodeId = $this->getEpisodeId($episode['episode_number'], $episodes);
+                        $line = sprintf("S%02dE%02d - %d - %s", $i, $episode['episode_number'], $tmdbEpisodeId, $episode['substitute_name']);
+                        $io->writeln($line);
+                        $substituteNames[] = [
+                            'serie_id' => $s['id'],
+                            'season_number' => $i,
+                            'episode_number' => $episode['episode_number'],
+                            'tmdb_episode_id' => $tmdbEpisodeId,
+                            'substitute_name' => $episode['substitute_name'],
+                        ];
+                    }
                 } else {
                     $line = sprintf("No substitute names found for season %d", $i);
                     $io->writeln($line);
@@ -150,10 +140,10 @@ class ExportEpisodeName extends Command
     private function getSeries($userId, $locale): ?array
     {
         $sql = "SELECT s.`id` as id, s.`name` as name, sln.`name` as localized_name, s.`serie_id` as tmdb_id, s.`number_of_seasons` as `number_of_seasons` "
-         .   "FROM `serie_viewing` sv "
-         .   "LEFT JOIN `serie` s ON s.`id`=sv.`serie_id` "
-         .   "LEFT JOIN `serie_localized_name` sln ON sln.`serie_id`=s.`id` AND sln.`locale`='$locale' "
-         .   "WHERE sv.`user_id`=$userId";
+            . "FROM `serie_viewing` sv "
+            . "LEFT JOIN `serie` s ON s.`id`=sv.`serie_id` "
+            . "LEFT JOIN `serie_localized_name` sln ON sln.`serie_id`=s.`id` AND sln.`locale`='$locale' "
+            . "WHERE sv.`user_id`=$userId";
 
         $em = $this->registry->getManager();
         $statement = $em->getConnection()->prepare($sql);
@@ -165,10 +155,10 @@ class ExportEpisodeName extends Command
     private function getOneSeries($userId, $serieId, $locale): ?array
     {
         $sql = "SELECT s.`id` as id, s.`name` as name, sln.`name` as localized_name, s.`serie_id` as tmdb_id, s.`number_of_seasons` as `number_of_seasons` "
-         .   "FROM `serie_viewing` sv "
-         .   "LEFT JOIN `serie` s ON s.`id`=sv.`serie_id` "
-         .   "LEFT JOIN `serie_localized_name` sln ON sln.`serie_id`=s.`id` AND sln.`locale`='$locale' "
-         .   "WHERE sv.`user_id`=$userId AND sv.`serie_id`=$serieId";
+            . "FROM `serie_viewing` sv "
+            . "LEFT JOIN `serie` s ON s.`id`=sv.`serie_id` "
+            . "LEFT JOIN `serie_localized_name` sln ON sln.`serie_id`=s.`id` AND sln.`locale`='$locale' "
+            . "WHERE sv.`user_id`=$userId AND sv.`serie_id`=$serieId";
 
         $em = $this->registry->getManager();
         $statement = $em->getConnection()->prepare($sql);
@@ -179,11 +169,13 @@ class ExportEpisodeName extends Command
 
     private function getSeasonEpisodes($userId, $serieId, $season_number): ?array
     {
-        $sql = "SELECT ep.`id` as id, ep.`episode_number` as episode_number, ep.`substitute_name` as substitute_name "
-         .   "FROM `episode_viewing` ep "
-         .   "INNER JOIN `serie_viewing` sv ON sv.`serie_id`=$serieId "
-         .   "INNER JOIN `season_viewing` sev ON sev.`serie_viewing_id`=sv.`id` "
-         .   "WHERE sv.`user_id`=$userId AND ep.`season_id`=sev.`id` AND sev.`season_number`=$season_number";
+        $sql = <<<SQL
+            SELECT ep.`id` as id, ep.`episode_number` as episode_number, ep.`substitute_name` as substitute_name
+            FROM `episode_viewing` ep 
+            INNER JOIN `serie_viewing` sv ON sv.`serie_id`=$serieId 
+            INNER JOIN `season_viewing` sev ON sev.`serie_viewing_id`=sv.`id` 
+            WHERE sv.`user_id`=$userId AND ep.`season_id`=sev.`id` AND sev.`season_number`=$season_number
+        SQL;
 
         $em = $this->registry->getManager();
         $statement = $em->getConnection()->prepare($sql);
