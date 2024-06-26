7e1870bbdd00527a53857bce77b79a030ac53e22
Yoli35 <ojm16@free.fr>
2024-06-26 14:26:43 +0200

    Alerts of the day: corrected query

--- src/Repository/AlertRepository.php
+++ src/Repository/AlertRepository.php
@@ -41,21 +41,23 @@ class AlertRepository extends ServiceEntityRepository
 
     public function alertOfTheDay($userId): array
     {
-        $sql = "SELECT a.`id` as id, a.`message` as message, a.`provider_id` as provider_id, a.`episode_number` as alert_episode_number, a.`season_number` as alert_season_number, "
-            . "        sv.`number_of_episodes` as number_of_episodes, sv.`number_of_seasons` as number_of_seasons, sv.`viewed_episodes` as viewed_episodes, "
-            . "        s.`name` as name, s.`original_name` as original_name, s.`serie_id` as tmdb_id, s.`origin_country` as origin_country_array,"
-            . "        s.`direct_link` as direct_link, "
-            . "        sln.`name` as localized_name, "
-            . "        se.`poster_path` as season_poster_path, ep.`still_path` as episode_still_path, "
-            . "        wp.`provider_name` as provider_name, wp.`logo_path` as provider_logo_path "
-            . "FROM `alert` a "
-            . "INNER JOIN `serie_viewing` sv ON sv.id=a.`serie_viewing_id` "
-            . "LEFT JOIN `serie` s ON s.id=sv.`serie_id` "
-            . "LEFT JOIN `serie_localized_name` sln ON sln.`serie_id`=s.`id` "
-            . "LEFT JOIN `season` se ON se.`series_id`=s.`id` AND se.`season_number`=a.`season_number` "
-            . "LEFT JOIN `episode` ep ON ep.`season_id`=se.`id` AND ep.`episode_number`=a.`episode_number` "
-            . "LEFT JOIN `watch_provider` wp ON wp.`provider_id` = a.`provider_id` "
-            . "WHERE a.`user_id`=" . $userId . " AND a.`activated`=1 AND ( (sv.`time_shifted`=0 AND DATE(a.`date`)=DATE(NOW()) ) OR( sv.`time_shifted`=1 AND DATE(a.`date`)=DATE_SUB(DATE(NOW()), INTERVAL 1 DAY)) )";
+        $sql = "SELECT a.`id` as id, a.`message` as message, a.`provider_id` as provider_id, a.`episode_number` as alert_episode_number, a.`season_number` as alert_season_number, 
+                    sv.`number_of_episodes` as number_of_episodes, sv.`number_of_seasons` as number_of_seasons, sv.`viewed_episodes` as viewed_episodes, 
+                    s.`name` as name, s.`original_name` as original_name, s.`serie_id` as tmdb_id, s.`origin_country` as origin_country_array,
+                    s.`direct_link` as direct_link, 
+                    sln.`name` as localized_name, 
+                    se.`poster_path` as season_poster_path, ep.`still_path` as episode_still_path, 
+                    wp.`provider_name` as provider_name, wp.`logo_path` as provider_logo_path 
+            FROM `alert` a 
+            INNER JOIN `serie_viewing` sv ON sv.id=a.`serie_viewing_id` 
+            LEFT JOIN `serie` s ON s.id=sv.`serie_id` 
+            LEFT JOIN `serie_localized_name` sln ON sln.`serie_id`=s.`id` 
+            LEFT JOIN `season` se ON se.`series_id`=s.`id` AND se.`season_number`=a.`season_number` 
+            LEFT JOIN `episode` ep ON ep.`season_id`=se.`id` AND ep.`episode_number`=a.`episode_number` 
+            LEFT JOIN `watch_provider` wp ON wp.`provider_id` = a.`provider_id` 
+            WHERE a.`user_id`=$userId
+              AND a.`activated`=1
+              AND DATE(`a`.`date`) = DATE(NOW())";
 
         return $this->registry->getManager()
             ->getConnection()->prepare($sql)
